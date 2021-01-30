57e9e6c1b86ba8919a010b638aab7832ff6568a2
Pedro Soares <GT01Pedro@thecea.ca>
2021-01-30 12:19:14 -0800

    uncompleted Feature

---
+++ GameJam2021/Assets/Scripts/Player/Flashlight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light​​​​​))]
+public class Flashlight : MonoBehaviour
+{
+
+    private Light​​​​​​​​​​​​​​​​​ _light​​​​​;
+
+    void Awake()
+    {
+        _light​​​​​ = GetComponent<Light​​​​​>();
+    }
+
+
+}
---
+++ GameJam2021/Assets/Scripts/Player/Upgrade.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UpgradeType
+{
+    Flashlight, MoveSpeed
+}
+public class Upgrade : MonoBehaviour, IUpgradable
+{
+    void IUpgradable.Upgrade()
+    {
+        throw new System.NotImplementedException();
+    }
+}

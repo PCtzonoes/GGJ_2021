7ce7208b7a26da164bd1155f9cc499a221ebdbef
Pedro Soares <GT01Pedro@thecea.ca>
2021-01-30 00:34:20 -0800

    Basic Player Movement
    And some lights

---
+++ GameJam2021/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 6;
+
+    private float _inputX;
+    private float _inputY;
+    private Rigidbody _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _inputY = Input.GetAxis("Vertical");
+        _inputX = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidBody.velocity = new Vector3(_inputX * _moveSpeed, 0, _inputY * _moveSpeed);
+    }
+}

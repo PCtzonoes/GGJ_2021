3e7d13554fb562915838cccf42cc68aea01eae3e
Richmond <richmond998999@yahoo.com>
2021-01-30 19:29:46 -0800

    Add code to exit the game. Indicates when active or inactive.

---
+++ GameJam2021/Assets/Scripts/Exit/Exit.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void LevelFinishHandler();
+
+public class Exit : MonoBehaviour
+{
+    private bool _active = false;
+    public bool active
+    {
+        get => _active;
+
+        set
+        {
+            if(value)
+            {
+                // TODO: Add Active State Change Here
+                GetComponent<MeshRenderer>().material.color = _activeColor;
+            }
+            else
+            {
+                // TODO: Add Inactive State Here
+                GetComponent<MeshRenderer>().material.color = _inactiveColor;
+            }
+            _active = value;
+        }
+
+    }
+
+    private Color _activeColor = Color.green;
+    private Color _inactiveColor = Color.red;
+    public event LevelFinishHandler LevelFinish;
+
+    void Start()
+    {
+        // TODO: Temp Inactive State change once we have object to render active
+        GetComponent<MeshRenderer>().material.color = _inactiveColor;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(_active && collision.gameObject.CompareTag("Player"))
+        {
+            LevelFinish();
+        }
+    }
+}

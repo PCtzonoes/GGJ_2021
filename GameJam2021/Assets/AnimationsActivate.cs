14f0e70d8ae1a2d3fd380858252b9b64cf0d40eb
MishaS3D <ferrariman1492@gmail.com>
2021-01-31 11:08:18 -0800

    Added Animations and collision
    Decided to add some animations and collision to the junk piles and a more obvious exit with junk that moves slowly...

---
+++ GameJam2021/Assets/AnimationsActivate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI;
+
+public class AnimationsActivate : MonoBehaviour
+
+   
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RuntimeAnimatorController.FindObjectOfType<Canvas>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameJam2021/Assets/Interactions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Interactions : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Player;
+    public GameObject Pile;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player.GetComponent<Collider>();
+
+        Pile.GetComponent<Collider>();
+
+        if (Pile.tag == ("Finish"))
+        {
+            print("Move me");
+        }
+
+    }
+}

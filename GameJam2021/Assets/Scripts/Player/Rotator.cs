db0ae0f2fc0894e50d0e6f99f83be8c16aeffe2b
Pedro Soares <GT01Pedro@thecea.ca>
2021-01-30 16:28:30 -0800

    Rotator Testing

--- GameJam2021/Assets/Scripts/Player/Rotator.cs
+++ GameJam2021/Assets/Scripts/Player/Rotator.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float _rotateSpeed = 1.0f;
+
+    private float _inputX;
+    private float _inputY;
+
+    private Vector3 _direction = Vector3.zero;
+
+    private void Awake()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _inputY = Input.GetAxis("Mouse Y");
+        _inputX = Input.GetAxis("Mouse X");
+    }
+
+    private void LateUpdate()
+    {
+        var input = new Vector3(_inputX, transform.position.y, _inputY);
+        _direction = Vector3.Lerp(_direction, input, _rotateSpeed);
+        transform.LookAt(_direction);
     }
 }
